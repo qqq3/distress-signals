88501f01d2a4f330c6efc5f6fe3233d24c7f4ec1
qqq3 <aiqcms@gmail.com>
2016-02-04 15:10:09 +0300

    Add activity-host FlashlightActivity

---
+++ app/src/main/java/org/asdtm/distresssignals/FlashlightActivity.java
@@ -0,0 +1,14 @@
+package org.asdtm.distresssignals;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class FlashlightActivity extends FragmentActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
