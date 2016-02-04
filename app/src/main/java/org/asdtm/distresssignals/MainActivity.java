cf2f58e46a006f9f0fe6f62c8d4045ecaeaab624
qqq3 <aiqcms@gmail.com>
2016-02-04 14:31:06 +0300

    Init toolbar and set alpha

--- app/src/main/java/org/asdtm/distresssignals/MainActivity.java
+++ app/src/main/java/org/asdtm/distresssignals/MainActivity.java
@@ -2,14 +2,17 @@ package org.asdtm.distresssignals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.getBackground().setAlpha(0);
     }
 }
