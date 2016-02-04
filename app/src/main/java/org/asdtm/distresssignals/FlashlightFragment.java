017cee79477986f20a52b667e113f9ecb43e09bf
qqq3 <aiqcms@gmail.com>
2016-02-04 17:06:24 +0300

    Check flash on device

--- app/src/main/java/org/asdtm/distresssignals/FlashlightFragment.java
+++ app/src/main/java/org/asdtm/distresssignals/FlashlightFragment.java
@@ -1,13 +1,20 @@
 package org.asdtm.distresssignals;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class FlashlightFragment extends Fragment
 {
+    private Camera mCamera;
+    private boolean isFlashOn;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -19,6 +26,18 @@ public class FlashlightFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_flashlight, parent, false);
 
+        if (!checkFlash(getActivity())) {
+
+            Toast.makeText(getActivity(), R.string.flashNotHasMessage, Toast.LENGTH_SHORT).show();
+
+        }
+
         return v;
+    }
+
+    private boolean checkFlash(Context context)
+    {
+        return context.getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA_FLASH);
     }
 }
