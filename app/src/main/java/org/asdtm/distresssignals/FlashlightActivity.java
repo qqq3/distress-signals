b52500f0ecc756f78ea2b5276416f40ca9e634a4
qqq3 <aiqcms@gmail.com>
2016-02-04 16:38:09 +0300

    Add flashlight fragment into FragmentManager and commit transaction

--- app/src/main/java/org/asdtm/distresssignals/FlashlightActivity.java
+++ app/src/main/java/org/asdtm/distresssignals/FlashlightActivity.java
@@ -1,7 +1,9 @@
 package org.asdtm.distresssignals;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class FlashlightActivity extends FragmentActivity
 {
@@ -10,5 +12,14 @@ public class FlashlightActivity extends FragmentActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragmentContainer = fragmentManager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragmentContainer == null) {
+            fragmentContainer = new FlashlightFragment();
+            fragmentManager.beginTransaction().add(R.id.fragmentContainer,
+                                                   fragmentContainer).commit();
+        }
     }
 }
