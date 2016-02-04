569da11f2491c387782577cb85cd80b5dc23bd06
qqq3 <aiqcms@gmail.com>
2016-02-04 15:02:40 +0300

    Add action on click start new activity button

--- app/src/main/java/org/asdtm/distresssignals/MainActivity.java
+++ app/src/main/java/org/asdtm/distresssignals/MainActivity.java
@@ -1,8 +1,10 @@
 package org.asdtm.distresssignals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -14,5 +16,11 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.getBackground().setAlpha(0);
+    }
+
+    public void startFlashlightActivity(View v)
+    {
+        Intent intent = new Intent(MainActivity.this, FlashlightActivity.class);
+        startActivity(intent);
     }
 }
