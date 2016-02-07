8203e81ae2a508266944a658c3128bd3cbb5248d
qqq3 <aiqcms@gmail.com>
2016-02-07 14:47:20 +0300

    Add LocationListener

--- app/src/main/java/org/asdtm/distresssignals/LocationFragment.java
+++ app/src/main/java/org/asdtm/distresssignals/LocationFragment.java
@@ -1,5 +1,9 @@
 package org.asdtm.distresssignals;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -19,6 +23,36 @@ public class LocationFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_location, parent, false);
 
+        LocationManager locationManager =
+                (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
         return v;
+    }
+
+    private LocationListener mLocationListener = new LocationListener()
+    {
+        @Override
+        public void onLocationChanged(Location location)
+        {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras)
+        {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider)
+        {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider)
+        {
+
+        }
     }
 }
