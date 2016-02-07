b268da825488d627e1d7f204277faca3d198977f
qqq3 <aiqcms@gmail.com>
2016-02-07 19:59:19 +0300

    Get fragment manager and commit CompassFragment

--- app/src/main/java/org/asdtm/distresssignals/CompassActivity.java
+++ app/src/main/java/org/asdtm/distresssignals/CompassActivity.java
@@ -1,7 +1,9 @@
 package org.asdtm.distresssignals;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class CompassActivity extends FragmentActivity
 {
@@ -10,5 +12,13 @@ public class CompassActivity extends FragmentActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CompassFragment();
+            fragmentManager.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
     }
 }
