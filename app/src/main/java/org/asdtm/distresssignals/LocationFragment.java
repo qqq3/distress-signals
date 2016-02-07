4f7c5237e42f3f53752b7cdbd73b6a2ec183b40f
qqq3 <aiqcms@gmail.com>
2016-02-07 15:39:06 +0300

    Add gpsRequestLocation method

--- app/src/main/java/org/asdtm/distresssignals/LocationFragment.java
+++ app/src/main/java/org/asdtm/distresssignals/LocationFragment.java
@@ -1,17 +1,24 @@
 package org.asdtm.distresssignals;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class LocationFragment extends Fragment
 {
+    LocationManager locationManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -23,7 +30,7 @@ public class LocationFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_location, parent, false);
 
-        LocationManager locationManager =
+        locationManager =
                 (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
         return v;
@@ -54,5 +61,28 @@ public class LocationFragment extends Fragment
         {
 
         }
+    };
+
+    public void gpsRequestLocation()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(getActivity(),
+                                                   Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                Toast.makeText(getActivity(), "Check app permission", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                                               mLocationListener);
     }
 }
