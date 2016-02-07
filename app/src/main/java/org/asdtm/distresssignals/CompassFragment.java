7b4ecfc1b23bc4f4d8d67d4ef1f33c2f0100a363
qqq3 <aiqcms@gmail.com>
2016-02-07 19:55:38 +0300

    Add compass activity, fragment and layout

---
+++ app/src/main/java/org/asdtm/distresssignals/CompassFragment.java
@@ -0,0 +1,24 @@
+package org.asdtm.distresssignals;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CompassFragment extends Fragment
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_compass, parent, false);
+
+        return v;
+    }
+}
