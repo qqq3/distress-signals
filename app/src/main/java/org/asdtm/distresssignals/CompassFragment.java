dee1c4092760664d38be34ef5546fca9cab0abb3
qqq3 <aiqcms@gmail.com>
2016-02-07 21:11:08 +0300

    Initialize compass image, compass degrees

--- app/src/main/java/org/asdtm/distresssignals/CompassFragment.java
+++ app/src/main/java/org/asdtm/distresssignals/CompassFragment.java
@@ -1,13 +1,22 @@
 package org.asdtm.distresssignals;
 
+import android.content.Context;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CompassFragment extends Fragment
 {
+    private ImageView mCompassImage;
+    private TextView mCompassDegree;
+
+    private SensorManager mSensorManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -18,6 +27,11 @@ public class CompassFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_compass, parent, false);
+
+        mCompassDegree = (TextView) v.findViewById(R.id.compass_degree);
+        mCompassImage = (ImageView) v.findViewById(R.id.compass_image);
+
+        mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
 
         return v;
     }
