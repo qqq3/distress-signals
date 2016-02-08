0c9b0310d0241bc2cda07e7688a25f571a044b7d
qqq3 <aiqcms@gmail.com>
2016-02-08 19:00:51 +0300

    Change compass

--- app/src/main/java/org/asdtm/distresssignals/CompassFragment.java
+++ app/src/main/java/org/asdtm/distresssignals/CompassFragment.java
@@ -1,21 +1,38 @@
 package org.asdtm.distresssignals;
 
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class CompassFragment extends Fragment
+public class CompassFragment extends Fragment implements SensorEventListener
 {
+    private static final String TAG = "CompassFragment";
     private ImageView mCompassImage;
-    private TextView mCompassDegree;
+    private TextView mAzimuth;
 
     private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+
+    private float[] valuesAccelerometer;
+    private float[] valuesMagneticField;
+
+    private float[] matrixR;
+    private float[] matrixI;
+    private float[] matrixValues;
+    private float mCurrentDegree = 0f;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -28,11 +45,86 @@ public class CompassFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_compass, parent, false);
 
-        mCompassDegree = (TextView) v.findViewById(R.id.compass_degree);
+        mAzimuth = (TextView) v.findViewById(R.id.azimuth);
         mCompassImage = (ImageView) v.findViewById(R.id.compass_image);
 
         mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        valuesAccelerometer = new float[3];
+        valuesMagneticField = new float[3];
+
+        matrixR = new float[9];
+        matrixI = new float[9];
+        matrixValues = new float[3];
 
         return v;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                System.arraycopy(event.values, 0, valuesAccelerometer, 0, 3);
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                System.arraycopy(event.values, 0, valuesMagneticField, 0, 3);
+                break;
+        }
+
+        boolean success = SensorManager.getRotationMatrix(matrixR,
+                                                          matrixI,
+                                                          valuesAccelerometer,
+                                                          valuesMagneticField);
+
+        if (success) {
+            SensorManager.getOrientation(matrixR, matrixValues);
+
+            float azimuthInRadians = matrixValues[0];
+            float azimuthInDegrees = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
+            Log.d(TAG, "azimuthInDegrees" + azimuthInDegrees);
+
+            RotateAnimation ra = new RotateAnimation(
+                    mCurrentDegree,
+                    -azimuthInDegrees,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f);
+
+            ra.setDuration(250);
+
+            ra.setFillAfter(true);
+
+            mCompassImage.startAnimation(ra);
+
+            mAzimuth.setText("Azimuth: " + String.valueOf(azimuthInDegrees));
+            mCurrentDegree = -azimuthInDegrees;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
     }
 }
