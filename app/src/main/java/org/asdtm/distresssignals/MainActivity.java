b190ec5ed2210330951274173665f39501ac1e50
qqq3 <aiqcms@gmail.com>
2016-02-14 20:19:55 +0300

    Change start activity buttons to anonim listener

--- app/src/main/java/org/asdtm/distresssignals/MainActivity.java
+++ app/src/main/java/org/asdtm/distresssignals/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -16,23 +17,41 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.getBackground().setAlpha(0);
-    }
 
-    public void startFlashlightActivity(View v)
-    {
-        Intent intent = new Intent(MainActivity.this, FlashlightActivity.class);
-        startActivity(intent);
-    }
+        ImageButton locationButton = (ImageButton) findViewById(R.id.ButtonLocationActivity);
+        ImageButton flashlightButton = (ImageButton) findViewById(R.id.ButtonFlashlightActivity);
+        ImageButton compassButton = (ImageButton) findViewById(R.id.ButtonCompassActivity);
 
-    public void startLocationActivity(View v)
-    {
-        Intent intent = new Intent(MainActivity.this, LocationActivity.class);
-        startActivity(intent);
-    }
+        locationButton.setImageResource(R.drawable.ic_location_button);
 
-    public void startCompassActivity(View v)
-    {
-        Intent intent = new Intent(MainActivity.this, CompassActivity.class);
-        startActivity(intent);
+        locationButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        flashlightButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, FlashlightActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        compassButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, CompassActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
